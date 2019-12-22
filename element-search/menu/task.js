aae5c1f3637530e1b08fd883dffb67be952f51d5
Aleksandr Melnikov <melnikov@tikhvin.org>
2019-12-23 00:05:36 +0300

    first commit menu

---
+++ element-search/menu/task.js
@@ -0,0 +1,17 @@
+function clearParentMenuActive(element) {
+  Array.from(element.closest(".menu_main").querySelectorAll(".menu_active")).forEach(element => {
+    element.classList.remove("menu_active");
+  });
+}
+
+function click() {
+  if (this.nextElementSibling.tagName == "UL") {
+    clearParentMenuActive(this);
+    this.nextElementSibling.classList.add("menu_active");
+  }
+  return false;
+}
+
+Array.from(document.getElementsByClassName("menu__link")).forEach( (element) => {
+  element.onclick = click;
+});
