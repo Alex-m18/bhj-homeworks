7e3a238b76ec1aa47c234722f1c841ee5babd234
Aleksandr Melnikov <melnikov@tikhvin.org>
2020-01-16 20:34:36 +0300

    Correction of comments on popup

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,24 +1,16 @@
-function doOnce(callback) {
-  if (
-    document.cookie.replace(
-      /(?:(?:^|.*;\s*)subcribeShown\s*\=\s*([^;]*).*$)|^.*$/,
-      "$1"
-    ) !== "true"
-  ) {
-    callback.apply(this, arguments);
-    document.cookie =
-      "subcribeShown=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
-  }
-}
-
 let showSubscribe = function() {
-  let subscribeElement = document.getElementById("subscribe-modal");
-  subscribeElement.classList.add("modal_active");
-  subscribeElement.addEventListener("click", function(e) {
-    this.classList.remove("modal_active");
-  });
-};
+  if (document.cookie.replace( /(?:(?:^|.*;\s*)subcribeShown\s*\=\s*([^;]*).*$)|^.*$/, "$1" ) !== "true") {
 
-showSubscribe = doOnce(showSubscribe);
+    let subscribeElement = document.getElementById("subscribe-modal");
+    subscribeElement.classList.add("modal_active");
+
+    subscribeElement.querySelector(".modal__close").addEventListener("click", function(e) {
+      this.closest(".modal").classList.remove("modal_active");
+      document.cookie =
+        "subcribeShown=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+    });
+
+  }
+};
 
 showSubscribe();
